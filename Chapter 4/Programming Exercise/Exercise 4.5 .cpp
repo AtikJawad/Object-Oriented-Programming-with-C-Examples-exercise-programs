15db2929fcbdacca242ca7fd268842d84b6024ec
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2024-09-05 00:36:20 +0600

    Create Exercise 4.5 .cpp

---
+++ Chapter 4/Programming Exercise/Exercise 4.5 .cpp
@@ -0,0 +1,15 @@
+//4.5: Write a macro that obtains the largest of the three numbers.
+#include <iostream>
+#define MAX(a, b, c) ((a > b && a > c) ? a : (b > c ? b : c))
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    cout << "The largest number is: " << MAX(a, b, c) << endl;
+
+    return 0;
+}
